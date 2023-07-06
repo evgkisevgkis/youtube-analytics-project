f729f6c8a2cbc64a44c38a64994f894ae66d23e0
Johnny <evgkisevgkis@gmail.com>
2023-07-06 19:55:40 +0500

    Модификация конструктора класса Channel

--- src/channel.py
+++ src/channel.py
@@ -11,8 +11,15 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.id = self.channel["items"][0]["id"]
+        self.title = self.channel["items"][0]["snippet"]["title"]
+        self.description = self.channel["items"][0]["snippet"]["description"]
+        self.url = f'https://www.youtube.com/{self.channel["items"][0]["snippet"]["customUrl"]}'
+        self.subscriber_count = self.channel["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
+        self.view_count = self.channel["items"][0]["statistics"]["viewCount"]
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        return print(json.dumps(channel, indent=2, ensure_ascii=False))
+        return print(json.dumps(self.channel, indent=2, ensure_ascii=False))
