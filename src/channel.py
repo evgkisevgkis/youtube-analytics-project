b57780072ae6076ce911df37157c956cfb4e32db
Johnny <evgkisevgkis@gmail.com>
2023-08-14 21:17:12 +0500

    Добавление магических методов для сложения, вычитания и сравнения

--- src/channel.py
+++ src/channel.py
@@ -16,13 +16,29 @@ class Channel:
         self.title = self.channel["items"][0]["snippet"]["title"]
         self.description = self.channel["items"][0]["snippet"]["description"]
         self.url = f'https://www.youtube.com/{self.channel["items"][0]["snippet"]["customUrl"]}'
-        self.subscriber_count = self.channel["items"][0]["statistics"]["subscriberCount"]
+        self.subscriber_count = int(self.channel["items"][0]["statistics"]["subscriberCount"])
         self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
         self.view_count = self.channel["items"][0]["statistics"]["viewCount"]
 
     def __str__(self):
         """Возвращает название канала и ссылку на него"""
         return f"{self.title} ({self.url})"
+
+    def __add__(self, other):
+        """Реализация сложения по подписчикам"""
+        return self.subscriber_count + other.subscriber_count
+
+    def __sub__(self, other):
+        """Реализация вычитания по подписчикам"""
+        return self.subscriber_count - other.subscriber_count
+
+    def __lt__(self, other):
+        """Реализация сравнения по подписчикам"""
+        return self.subscriber_count < other.subscriber_count
+
+    def __le__(self, other):
+        """Реализация сравнения по подписчикам"""
+        return self.subscriber_count <= other.subscriber_count
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
