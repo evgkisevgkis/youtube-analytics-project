8e2b1a5d7d0bf34ce5f3eea58a7b95b8a499705e
Johnny <evgkisevgkis@gmail.com>
2023-08-28 15:46:31 +0500

    Добавление класса Video

---
+++ src/video.py
@@ -0,0 +1,21 @@
+import json
+import os
+from googleapiclient.discovery import build
+
+
+api_key: str = os.getenv('YT_API_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+
+class Video:
+    """Класс для видео"""
+    def __init__(self, video_id: str):
+        self.video_id = video_id
+        self.video_response = youtube.videos().list(part='snippet,'
+                                                         'statistics,contentDetails,topicDetails',
+                                                    id=self.video_id).execute()
+        self.video_title: str = self.video_response['items'][0]['snippet']['title']
+        self.url = f'https://www.youtube.com/{self.video_id}'
+        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+        self.comment_count: int = self.video_response['items'][0]['statistics']['commentCount']
