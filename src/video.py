6576c02988d5740b18b7224d4073ff796a86c82a
Johnny <evgkisevgkis@gmail.com>
2023-09-25 21:09:48 +0500

    Добавление обработки исключений при неправильном id

--- src/video.py
+++ src/video.py
@@ -10,14 +10,22 @@ class Video:
     """Класс для видео"""
     def __init__(self, video_id: str):
         self.video_id = video_id
-        self.video_response = youtube.videos().list(part='snippet,'
-                                                         'statistics,contentDetails,topicDetails',
-                                                    id=self.video_id).execute()
-        self.video_title: str = self.video_response['items'][0]['snippet']['title']
-        self.url = f'https://www.youtube.com/{self.video_id}'
-        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
-        self.comment_count: int = self.video_response['items'][0]['statistics']['commentCount']
+        try:
+            self.video_response = youtube.videos().list(part='snippet,'
+                                                             'statistics,contentDetails,topicDetails',
+                                                        id=self.video_id).execute()
+            self.video_title: str = self.video_response['items'][0]['snippet']['title']
+            self.url = f'https://www.youtube.com/{self.video_id}'
+            self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+            self.comment_count: int = self.video_response['items'][0]['statistics']['commentCount']
+        except IndexError:
+            self.video_response = None
+            self.video_title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
+            self.comment_count = None
 
     def __str__(self):
         return self.video_title
